68becd2369287a1669a58f8e123ea206a7609498
Mainak Karmakar <iammainak58@gmail.com>
2024-07-26 16:52:13 +0000

    iife

--- function/6_iife.js
+++ function/6_iife.js
@@ -1,5 +1,32 @@
-// Immediately Invoked Function Expression
+// // Immediately Invoked Function Expression
+// // to execute immidiately 
+// // for interview to stop polluting from global scope
 
-function chai(){
+
+// //iife we use not to polute in locan fuction
+
+// function chai(){
+//     console.log(`DB Connected`)
+// }
+// chai()
+
+
+// how to use iife
+
+(function chai(){
     console.log(`DB Connected`)
-}
+})() ;
+
+// ()FOR function and 2nd() for execution
+
+//arrow function
+(() =>{
+    console.log(`DB connect 2`) 
+}) () ;
+
+
+//name
+
+((name) =>{
+    console.log(`DB connect 2 ${name}`) 
+}) ("mainak") ;
